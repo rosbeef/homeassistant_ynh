8afb5c984b3bf21f601c4d643a9a145918a7fa08
rosbeef andino <romain.raynaud@3cmr.fr>
2023-10-06 18:04:39 +0000

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -142,6 +142,9 @@ myynh_install_homeassistant () {
 		# install last version of pip
 		ynh_exec_warn_less ynh_exec_as $app "$install_dir/bin/pip3" --cache-dir "$data_dir/.cache" install --upgrade "$pip_required"
 
+  		# install cython < 3.0 to make install works with pyyaml6.0
+		ynh_exec_warn_less ynh_exec_as $app "$install_dir/bin/pip3" --cache-dir "$data_dir/.cache" install --upgrade "$pip_required"
+
 		# install last version of wheel
 		ynh_exec_warn_less ynh_exec_as $app "$install_dir/bin/pip3" --cache-dir "$data_dir/.cache" install --upgrade wheel
 
