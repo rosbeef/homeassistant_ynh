81809a08771c0255ee3bed5f9e8b94b3d9e48861
rosbeef andino <romain.raynaud@3cmr.fr>
2023-12-20 21:00:29 +0000

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -202,7 +202,10 @@ myynh_install_homeassistant () {
 		# install last version of setuptools
 		ynh_exec_warn_less ynh_exec_as $app "$install_dir/bin/pip3" --cache-dir "$data_dir/.cache" install --upgrade setuptools
 
-		# install last version of mysqlclient
+		#ynh_exec_warn_less ynh_exec_as $app "$install_dir/bin/pip3" --cache-dir "$data_dir/.cache" install --upgrade ha-av
+		ynh_exec_warn_less ynh_exec_as $app "$install_dir/bin/pip3" --cache-dir "$data_dir/.cache" install --upgrade "opencv-python-headless"
+		
+  		# install last version of mysqlclient
 		ynh_exec_warn_less ynh_exec_as $app "$install_dir/bin/pip3" --cache-dir "$data_dir/.cache" install --upgrade mysqlclient
 		
 		# install Home Assistant
